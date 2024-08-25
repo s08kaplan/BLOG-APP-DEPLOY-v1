6563d7106f1cedaef3c7f9a45479c863a02829f7
Serkan <s8ersis@gmail.com>
2024-08-25 05:00:02 +0300

    vercel json added

--- api/index.js
+++ api/index.js
@@ -2,13 +2,13 @@
 
 const express = require("express");
 const app = express();
-const path = require("path");
+
 const cors = require("cors");
 
 require("dotenv").config();
 
 const HOST = process.env.HOST || "127.0.0.1";
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 8000;
 
 require("express-async-errors"); //* async-errors to errorHandler
 
@@ -23,7 +23,6 @@ app.use(cors());
 
 // Call static uploadFile:
 app.use("/upload", express.static("./upload"));
-app.use(express.static(path.join(__dirname, "public")));
 
 // Run Logger:
 // app.use(require('./src/middlewares/logger'))
@@ -37,27 +36,17 @@ app.all("/api", (req, res) => {
     error: false,
     message: "Welcome to Blog API",
     documents: {
-      swagger: "/api/documents/swagger",
-      redoc: "/api/documents/redoc",
-      json: "/api/documents/json",
+      swagger: "/documents/swagger",
+      redoc: "/documents/redoc",
+      json: "/documents/json",
     },
     user: req.user,
   });
 });
 
 //* Routes:
-app.use("/api", require("./src/routes"));
+app.use(require("./src/routes"));
 
-// !deploy-----------------------------------------
-app.use("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./public", "index.html"));
-});
-
-app.use("*", (req, res) => {
-  res.status(404).send({ message: "Not Found" });
-});
-
-//! -----------------------------------------------
 
 //*error handler
 app.use(require("./src/middlewares/errorHandler"));
