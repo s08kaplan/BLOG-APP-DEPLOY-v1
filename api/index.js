d1b0d8222cd930b9e186d00f5abf809198a1d9c6
Serkan <s8ersis@gmail.com>
2024-08-25 04:50:46 +0300

    repo check

--- api/index.js
+++ api/index.js
@@ -1,68 +1,69 @@
- "use strict"
- 
- const express = require("express")
- const app = express()
- const path = require("path")
- const cors = require("cors")
+"use strict";
 
- require("dotenv").config()
+const express = require("express");
+const app = express();
+const path = require("path");
+const cors = require("cors");
 
- const HOST = process.env.HOST || "127.0.0.1"
- const PORT = process.env.PORT || 10000 
+require("dotenv").config();
 
- require("express-async-errors")  //* async-errors to errorHandler
+const HOST = process.env.HOST || "127.0.0.1";
+const PORT = process.env.PORT || 10000;
 
- //* MONGODB Connection
- const { dbConnection } = require("./src/configs/dbConnection")
- dbConnection()
+require("express-async-errors"); //* async-errors to errorHandler
 
- //* JSON for data interchange
- app.use(express.json())
- app.use(express.urlencoded({extended:true}))
- app.use(cors())
+//* MONGODB Connection
+const { dbConnection } = require("./src/configs/dbConnection");
+dbConnection();
 
- // Call static uploadFile:
-app.use('/upload', express.static('./upload'))
-app.use(express.static(path.join(__dirname, "public")))
+//* JSON for data interchange
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+// Call static uploadFile:
+app.use("/upload", express.static("./upload"));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Run Logger:
 // app.use(require('./src/middlewares/logger'))
 
- // Check Authentication:
-app.use(require('./src/middlewares/authentication'))
-app.use(require("./src/middlewares/queryHandler"))
+// Check Authentication:
+app.use(require("./src/middlewares/authentication"));
+app.use(require("./src/middlewares/queryHandler"));
 
-app.all('/api', (req, res) => {
+app.all("/api", (req, res) => {
   res.send({
-      error: false,
-      message: 'Welcome to Blog API',
-      documents: {
-          swagger: '/api/documents/swagger',
-          redoc: '/api/documents/redoc',
-          json: '/api/documents/json',
-      },
-      user: req.user
-  })
-})
+    error: false,
+    message: "Welcome to Blog API",
+    documents: {
+      swagger: "/api/documents/swagger",
+      redoc: "/api/documents/redoc",
+      json: "/api/documents/json",
+    },
+    user: req.user,
+  });
+});
 
 //* Routes:
-app.use("/api",require("./src/routes"))
+app.use("/api", require("./src/routes"));
 
 // !deploy-----------------------------------------
 app.use("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname,"./public", "index.html"))
-})
+  res.sendFile(path.resolve(__dirname, "./public", "index.html"));
+});
 
 app.use("*", (req, res) => {
-  res.status(404).send({message: "Not Found"})
-})
+  res.status(404).send({ message: "Not Found" });
+});
 
 //! -----------------------------------------------
 
+//*error handler
+app.use(require("./src/middlewares/errorHandler"));
 
- //*error handler
- app.use(require("./src/middlewares/errorHandler"))
-
- app.listen(PORT, () => console.log(`Server is running on http://${process.env.HOST}:${PORT}`))
+app.listen(PORT, () =>
+  console.log(`Server is running on http://${process.env.HOST}:${PORT}`)
+);
 
 //  require("./src/helpers/sync")()  //! it clears the whole database
